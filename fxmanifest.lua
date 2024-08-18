f2a260b56b4962ad720134f7e495d225769764d5
PanScoobiik <68381744+PanScoobiik@users.noreply.github.com>
2024-08-18 18:33:02 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Scoob'
+version 'v1'
+
+loadscreen_cursor 'yes'
+
+loadscreen 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/script.js',
+    'html/style.css',
+    'html/song.mp3',
+    'html/img/*',
+}
+
+
